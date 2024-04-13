41d8d9c337a5f202f8dd45296a7036dee10b58f8
Cold-Banana53 <140571519+Cold-Banana53@users.noreply.github.com>
2024-04-14 01:01:37 +0800

    changes
    re-added some lost progress

--- mainScript.js
+++ mainScript.js
@@ -1,16 +1,27 @@
 const loginForm = document.getElementById('login-form');
+const signupForm = document.getElementById('signup-form');
 const message = document.getElementById('message');
+
+loginForm.addEventListener('confirm', (event) => {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    if (usernameList.includes(username)) {
+        alert("Success! Redirecting to main...");
+        window.location.href = './nice.html';
+    } else {
+        alert('This username already exists!');
+    }
+});
 
 loginForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    const usernameList = [];
-    const passwordList = []; //don't search here until username check
     if (usernameList.includes(username) == false) {
         usernameList[usernameList.length] = username;
         passwordList[passwordList.length] = password;
-            alert("success! bringing you back to the login page...");
+            alert("Success! bringing you back to the login page...");
             window.location.href = './LoginPage.html';
     } else {
         alert('This username already exists!');
